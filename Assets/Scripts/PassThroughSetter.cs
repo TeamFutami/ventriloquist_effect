6fc9401bee79c6cffe5f67fbf9605405d2523b95
kyochn <is0586vi@ed.ritsumei.ac.jp>
2023-06-20 00:04:41 +0900

    パススルー切り替え実装

--- Assets/Scripts/PassThroughSetter.cs
+++ Assets/Scripts/PassThroughSetter.cs
@@ -1,20 +1,34 @@
+using System;
 using Unity.XR.PXR;
 using UnityEngine;
+using Photon.Pun;
 
-public class PassThroughSetter : MonoBehaviour
+public class PassThroughSetter : MonoBehaviourPunCallbacks
 {
-    private void Start()
+    [SerializeField] private Camera vrCamera;
+
+    public void EnableSeeThrough()
     {
-        // シースルー設定
+        photonView.RPC("RPCEnableSeeThrough", RpcTarget.Others);
+    }
+    
+    public void DisableSeeThrough()
+    {
+        photonView.RPC("RPCDisableSeeThrough", RpcTarget.Others);
+    }
+
+    [PunRPC]    
+    private async void RPCEnableSeeThrough()
+    {
+        vrCamera.clearFlags = CameraClearFlags.SolidColor;
+        vrCamera.backgroundColor = Color.clear;
         PXR_Boundary.EnableSeeThroughManual(true);   
     }
     
-    private void OnApplicationPause(bool pauseStatus)
+    [PunRPC]
+    private void RPCDisableSeeThrough()
     {
-        // Pauseからの復帰時、再度シースルーにする
-        if (!pauseStatus)
-        {
-            PXR_Boundary.EnableSeeThroughManual(true);
-        }
+        PXR_Boundary.EnableSeeThroughManual(false);
+        vrCamera.clearFlags = CameraClearFlags.Skybox;
     }
 }
